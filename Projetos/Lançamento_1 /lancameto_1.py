503fe12a5ea3dac23f42c99a1b1904dfede92032
G0 <118084068+G0ndim@users.noreply.github.com>
2023-09-23 02:24:00 -0300

    Update lancameto_1.py

--- Projetos/Lançamento_1 /lancameto_1.py
+++ Projetos/Lançamento_1 /lancameto_1.py
@@ -3,21 +3,21 @@ from MyLibs.utils import *
 
 while True:
 
-    # Menu com as formas geometricas disponiveis
+    # Menu com as formas geométricas disponíveis
     figuras = {1: Poligono, 2: Triangulo, 3: Quadrilatero, 4: Pentagono, 5: Hexagono}
     menu_inicial(figuras)
 
-    # Escolha do usuario entre os indices do menu
+    # Escolha do usuário entre os índices do menu
     opcao = int(input(f">> "))
 
-    # Entrada dos vertices
+    # Entrada dos vértices
     eixo_x, eixo_y, n = [], [], [0]
     coletor_vertices(opcao, n, eixo_x, eixo_y)
     forma = figuras[opcao](eixo_x, eixo_y, n[0])
 
-    # Menu opcao de troca dos vertices
+    # Menu opção de troca dos vértices
     troca_vertice(forma)
 
-    # resultado final
+    # Resultado dos cálculos
     resultado(forma)
 
