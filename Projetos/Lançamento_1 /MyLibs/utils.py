29be676e93acb02b93a79e7cf36826b9127fa107
G0ndim <pfgondim2004@gmail.com>
2023-09-25 22:59:01 -0400

    Customizacao do menu interativo

--- Projetos/Lançamento_1 /MyLibs/utils.py
+++ Projetos/Lançamento_1 /MyLibs/utils.py
@@ -1,15 +1,54 @@
 
+def limpa_terminal():
+    from subprocess import run
+    from platform import system, release
+    if system() == "Windows":
+        if release() in {"10", "11"}:
+            run("", shell=True)
+            print("\033c", end="")
+        else:
+            run(["cls"])
+    else:  # Linux and Mac
+        print("\033c", end="")
+
+
 def menu_inicial(figuras):
 
-    print(f"{'=-=' * 15}")
-    print(f"{'Calculadora Geometrica Bidimensional':^45}".upper())
-    print(f"{'=-=' * 15}")
-    print(f"{'Escolha uma figura':^45}")
+    print(f"\033[32m{'=-=' * 15}")
+    print(f"\033[32m{'Calculadora Geometrica Bidimensional':^45}".upper())
+    print(f"\033[32m{'=-=' * 15}")
+    print(f"\033[32m{'Escolha uma das opções':^45}")
     for i, j in figuras.items():
-        a = f"{i}. {j.__str__()}"
-        print(f"{a:<10}")
-    print(f"{'---' * 15}")
+        a = f"\033[36m{i}. \033[32m{j.__str__()}"
+        print(f"\033[32m{a:<10}")
+    print(f"\033[32m{'---' * 15}")
     pass
+
+
+def menu_selecao(figuras):
+
+    while True:
+        opcao = int(input(f"\033[36m>>(n): "))
+        limpa_terminal()
+        print(f"\033[32m{'=-=' * 15}")
+        print(f"\033[32m{'Calculadora Geometrica Bidimensional':^45}".upper())
+        print(f"\033[32m{'=-=' * 15}")
+        print(f"\033[32m{'Opçao escolhida':^45}")
+        for i, j in figuras.items():
+            if i == opcao:
+                a = f"\033[36m{i}. \033[7;32m{j.__str__()}\033[0m"
+            else:
+                a = f"\033[36m{i}. \033[32m{j.__str__()}"
+            print(f"\033[32m{a:<10}")
+        print(f"\033[32m{'---' * 15}")
+        if input("Deseja selecionar outra opcao?(s/n): ") in "Nn":
+            limpa_terminal()
+            print(f"\033[32m{'=-=' * 15}")
+            print(f"\033[32m{figuras[opcao].__str__():^45}".upper())
+            print(f"\033[32m{'=-=' * 15}")
+            break
+
+    return opcao
 
 
 def organizador_de_lista(lista_x, lista_y):
@@ -46,18 +85,44 @@ def organizador_de_lista(lista_x, lista_y):
     pass
 
 
-def coletor_vertices(num1, num_f, lista_x, lista_y):
+def coletor_vertices(num1, num_f, lista_x, lista_y, figuras, opcao):
 
     if num1 != 1:
         num_f[0] = num1 + 1
     else:
-        num_f[0] = int(input("Quantos vértices você quer digitar?: "))
+        print("\033[32mQuantos vértices você quer digitar?")
+        num_f[0] = int(input("\033[36m>>(n): "))
+        print(f"\033[32m{'---' * 15}")
 
+    vertices = []
     x, y = [], []
-
     for i in range(num_f[0]):
-        x.append(float(input(f"Digite o valor de x{i + 1}: ")))
-        y.append(float(input(f"Digite o valor de y{i + 1}: ")))
+        vertices.append([f"x{i + 1}", f"y{i + 1}"])
+
+    for i in range(num_f[0] * 2):
+        limpa_terminal()
+        print(f"\033[32m{'=-=' * 15}")
+        print(f"\033[32m{figuras[opcao].__str__():^45}".upper())
+        print(f"\033[32m{'=-=' * 15}")
+        print(f"\033[32mPreencha as coordenadas dos \033[36m{num_f[0]} \033[32mvertices a seguir: ")
+        print()
+        counter = 0
+        for j in range(len(vertices)):
+            for k in range(2):
+                if counter == i:
+                    print(f"\033[7;36m{vertices[j][k]}\033[0m", end=' ')
+                    a, b = j, k
+                else:
+                    print(f"\033[32m{vertices[j][k]}", end=' ')
+                counter += 1
+        print()
+        coordenada = float(input(f"\033[36m>>({vertices[a][b]}): "))
+        vertices[a][b] = coordenada
+        if b == 0:
+            x.append(vertices[a][b])
+        else:
+            y.append(vertices[a][b])
+        print()
 
     organizador_de_lista(x, y)
 
@@ -68,17 +133,70 @@ def coletor_vertices(num1, num_f, lista_x, lista_y):
     pass
 
 
-def troca_vertice(classe):
+def menu_vertices(classe, figuras, opcao):
+    limpa_terminal()
+    print(f"\033[32m{'=-=' * 15}")
+    print(f"\033[32m{figuras[opcao].__str__():^45}".upper())
+    print(f"\033[32m{'=-=' * 15}")
+    print(f"\033[32m{'Vértices':^45}")
+    for i, j in classe.localizacoes().items():
+        a = f"\033[36m{i}. \033[32m{j.__str__()}"
+        print(f"\033[32m{a:<10}")
+    print(f"\033[32m{'---' * 15}")
+
+
+def troca_vertice(classe, figuras, opcao):
     while True:
-        print(f'Vértices: {classe.localizacoes()}')
+        menu_vertices(classe, figuras, opcao)
         if input("Deseja trocar algum valor?(s/n): ").strip()[0] in 'Ss':
-            posicao = int(input("Digite a posição do vértice a ser trocado:"))
-            x_nov = float(input(f"Digite o novo valor de x{posicao}: "))
-            y_nov = float(input(f"Digite o novo valor de y{posicao}: "))
-            print(f"({classe.crd_x[posicao - 1]}, {classe.crd_y[posicao - 1]})  --->  ({x_nov}, {y_nov})")
-            resposta = input("Quer manter as alterações?(s/n)")
+            limpa_terminal()
+            print(f"\033[32m{'=-=' * 15}")
+            print(f"\033[32m{figuras[opcao].__str__():^45}".upper())
+            print(f"\033[32m{'=-=' * 15}")
+            print(f"\033[32m{'Digite a posição do vértice a ser trocado':^45}")
+            for i, j in classe.localizacoes().items():
+                a = f"\033[36m{i}. \033[32m{j.__str__()}"
+                print(f"\033[32m{a:<10}")
+            print(f"\033[32m{'---' * 15}")
+            while True:
+                posicao = int(input("\033[36m>>(n): \033[0m"))
+                limpa_terminal()
+                print(f"\033[32m{'=-=' * 15}")
+                print(f"\033[32m{figuras[opcao].__str__():^45}".upper())
+                print(f"\033[32m{'=-=' * 15}")
+                print(f"\033[32m{'Opcao selecionada':^45}")
+                for i, j in classe.localizacoes().items():
+                    if i == posicao:
+                        a = f"\033[36m{i}. \033[7;32m{j}\033[0m"
+                    else:
+                        a = f"\033[36m{i}. \033[32m{j}"
+                    print(f"\033[32m{a:<10}")
+                print(f"\033[32m{'---' * 15}")
+                if input("Deseja selecionar outra opcao?(s/n): ") in "Nn":
+                    break
+
+            for i in range(2):
+                limpa_terminal()
+                print(f"\033[32m{'=-=' * 15}")
+                print(f"\033[32m{figuras[opcao].__str__():^45}".upper())
+                print(f"\033[32m{'=-=' * 15}")
+                print(f"\033[32m{'Digite os novos valores':^45}")
+                if i == 0:
+                    a = f"(\033[7;32m{classe.localizacoes()[posicao][0]}\033[0;32m, {classe.localizacoes()[posicao][1]})"
+                    print(f"{a:^45}")
+                    print(f"\033[32m{'---' * 15}")
+                    x_nov = float(input(f"\033[36m>>(n): "))
+                else:
+                    a = f"({classe.localizacoes()[posicao][0]}, \033[7;32m{classe.localizacoes()[posicao][1]}\033[0;32m)"
+                    print(f"{a:^45}")
+                    print(f"\033[32m{'---' * 15}")
+                    y_nov = float(input(f"\033[36m>>(n): "))
+
+            print(f"\033[0;32m({classe.crd_x[posicao - 1]}, {classe.crd_y[posicao - 1]})  --->  ({x_nov}, {y_nov})")
+            resposta = input("Quer manter as alterações?(s/n): ")
             if resposta.strip()[0] in 'Ss':
                 classe.troca_coordenada(posicao - 1, x_nov, y_nov)
+            organizador_de_lista(classe.crd_x, classe.crd_y)
         else:
             break
     pass
@@ -91,3 +209,5 @@ def resultado(classe):
     # print(classe.verifica_convexo())
     print(classe.tipo())
     pass
+
+# (6.0, 0.0), 2: (0.0, 0.0), 3: (8.0, 4.0), 4: (2.0, 4.0)
--- Projetos/Lançamento_1 /lancameto_1.py
+++ Projetos/Lançamento_1 /lancameto_1.py
@@ -3,21 +3,36 @@ from MyLibs.utils import *
 
 while True:
 
-    # Menu com as formas geométricas disponíveis
+    limpa_terminal()
+    # Menu com as formas geometricas disponiveis
     figuras = {1: Poligono, 2: Triangulo, 3: Quadrilatero, 4: Pentagono, 5: Hexagono}
     menu_inicial(figuras)
 
-    # Escolha do usuário entre os índices do menu
-    opcao = int(input(f">> "))
+    opcao = menu_selecao(figuras)
 
-    # Entrada dos vértices
+    # Entrada dos vertices
     eixo_x, eixo_y, n = [], [], [0]
-    coletor_vertices(opcao, n, eixo_x, eixo_y)
+    coletor_vertices(opcao, n, eixo_x, eixo_y, figuras, opcao)
     forma = figuras[opcao](eixo_x, eixo_y, n[0])
 
-    # Menu opção de troca dos vértices
-    troca_vertice(forma)
+    # Menu opcao de troca dos vertices
+    troca_vertice(forma, figuras, opcao)
+    print(f"\033[32m{'---' * 15}")
 
-    # Resultado dos cálculos
+    # resultado final
     resultado(forma)
+    print(f"\033[32m{'---' * 15}")
+    if input("Deseja continuar?(s/n): ")[0] in "Nn":
+        print("Desligando calculadora")
+        break
 
+
+    """
+    menu_inicial(figuras)
+
+    # Escolha do usuario entre os indices do menu
+    opcao = int(input(f"\033[36m>>(n): "))
+    print(f"\033[32mOpcao selecionada: \033[36m{figuras[opcao].__str__()}")
+    print(f"\033[32m{'---' * 15}")
+
+    """
