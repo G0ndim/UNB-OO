e2ae96367df415078b4fc8801e16687d032ca5cb
G0ndim <pfgondim2004@gmail.com>
2023-12-19 12:24:30 -0400

    change in controlers and models

--- Projetos/TerminalInvaders/MyLibs/Controlers.py
+++ Projetos/TerminalInvaders/MyLibs/Controlers.py
@@ -39,8 +39,101 @@ class Round:
             self.condicao_vitoria = True
 
 
+class Arena:
+    def __init__(self, player, enemys, x_tela, y_tela, pontuacao):
+        self.round1 = Round(player, enemys, x_tela, y_tela)
+        self.pontuacao = pontuacao
+        self.x_tela = x_tela
+        self.y_tela = y_tela
+        self.count = 0
+
+    def play(self):
+        pygame.init()
+        tela = pygame.display.set_mode((self.x_tela, self.y_tela))
+        pygame.display.set_caption(f'Terminal Invaders')
+        clock = pygame.time.Clock()
+        timer_event = pygame.USEREVENT + 1
+        timer_interval = 1000
+        timer_event2 = pygame.USEREVENT + 2
+        timer_interval2 = 600
+
+        pygame.time.set_timer(timer_event, timer_interval)
+
+        self.round1.config_enemys()
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+                self.round1.player.atacar(timer_event, event)
+
+                for enemy_mage in self.round1.grupo_magos.sprites():
+                    enemy_mage.fireball_attack(self.round1.player.pos_x, self.round1.player.pos_y, timer_event, event)
+
+                if event.type == timer_event2:
+                    self.round1.player.invulnerabilidade = False
+
+            horda = [self.round1.grupo_zumbis.sprites(),
+                     self.round1.grupo_morcegos.sprites(),
+                     self.round1.grupo_magos.sprites(),
+                     self.round1.grupo_slime_king.sprites(),
+                     self.round1.grupo_slime.sprites()]
+
+            tela.fill((48, 10, 36))  # Cor de Fundo da Tela
+
+            for enemy_type in horda:
+                for inimigo in enemy_type:
+                    inimigo.update(tela, self.round1.player.pos_x, self.round1.player.pos_y)
+
+            self.round1.player.weapons[self.round1.player.weapon_number].bullet_update(tela)
+
+            for enemy_mage in self.round1.grupo_magos.sprites():
+                enemy_mage.fireball_update(tela)
+
+            for enemy_type in horda:
+                for inimigo in enemy_type:
+                    inimigo.attack(self.round1.player, timer_event2, timer_interval2)
+                    for bullet in self.round1.player.weapons[self.round1.player.weapon_number].bullet_group.sprites():
+                        self.count = inimigo.estado(bullet, self.round1.player.weapons[self.round1.player.weapon_number].dano)
+                        if isinstance(self.count, tuple):
+                            self.round1.grupo_slime.add(Slime(x_tela, y_tela, self.count[0], self.count[1]))
+                            self.round1.grupo_slime.add(Slime(x_tela, y_tela, self.count[0] + 9, self.count[1] + 30))
+                            self.round1.grupo_slime.add(Slime(x_tela, y_tela, self.count[0] + 14, self.count[1] - 30))
+                        elif isinstance(self.count, int):
+                            self.pontuacao += self.count
+                        self.count = 0
+
+            font = pygame.font.SysFont(None, 18)
+            tela.blit(font.render(f'{self.pontuacao}', True, (255, 255, 255)), (x_tela - 50, 6))
+
+            self.round1.player.update(tela)
+
+            pygame.display.update()
+            clock.tick(60)
+
+
 if __name__ == '__main__':
+
+    x_tela = 600
+    y_tela = 600
+    player_health = 100
+    player_velocity = 0.75
+    pistol_damage = 20
+    shotgun_damage = 10
+    sniper_damage = 90
+    shotgun_enable = True
+    sniper_enable = True
+    player = [player_health, player_velocity, pistol_damage, shotgun_damage, sniper_damage, shotgun_enable, sniper_enable]
+    enemys1 = [5, 2, 3, 1, 0]  # 5 zumbis, 2 morcegos, 3 magos, 1 slime_king, 0 slimes (sao instanciados na morte do king)
+    pontuacao = 500  # exemplo de pontuacao que foi obtida em round anterior
+    arena = Arena(player, enemys1, x_tela, y_tela, pontuacao)
+    arena.play()
+
+
+"""
     pygame.init()
+    
     x_tela = 600
     y_tela = 600
     tela = pygame.display.set_mode((x_tela, y_tela))
@@ -50,8 +143,8 @@ if __name__ == '__main__':
     player_velocity = 0.75
     player = [player_health, player_velocity, 20, 10, 90, True, True]
     enemys1 = [1, 1, 1, 1, 0]
-    round1 = Round(player, enemys1, x_tela, y_tela)
     pontuacao = 0
+    round1 = Round(player, enemys1, x_tela, y_tela)
     timer_event = pygame.USEREVENT + 1
     timer_interval = 1000
     timer_event2 = pygame.USEREVENT + 2
@@ -112,3 +205,4 @@ if __name__ == '__main__':
 
         pygame.display.update()
         clock.tick(60)
+"""
